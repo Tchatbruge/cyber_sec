24705bf9b4c47daf51636255db600218d8c2edeb
Tchtbruge <bruge.tchatchoua@student.unamur.be>
2025-02-03 22:09:57 +0100

    substitution cipher

--- my_test.py
+++ my_test.py
@@ -5,9 +5,26 @@ def test(mess):
             ans += i 
     return ans.lower()
 
-mes = "abCDefgh"
-print(test(mes))
+def count_occurence(text):
+    liste = {}
+    for i in text: 
 
-liste = [(ord(char) + 2) for char in mes ]
-listes = ''.join([(chr(char)) for char in liste ])
-print(listes)
+        liste[i] = text.count(i)
+    return liste
+
+def replace(text):
+    liste = ""
+    for i in text: 
+        if i == "Q" :
+            liste += "T" 
+        else :
+            liste += i
+
+    return liste
+
+mes = "CRYPTOGRAPHICSYSTEMSAREEXTREMELYDIFFICULTTOBUILDNEVERTHELESSFORSOMEREASONMANYNONEXPERTSINSISTONDESIGNINGNEWENCRYPTIONSCHEMESTHATSEEMTOTHEMTOBEMORESECURETHANANYOTHERSCHEMEONEARTHTHEUNFORTUNATETRUTHHOWEVERISTHATSUCHSCHEMESAREUSUALLYTRIVIALTOBREAK"
+print(count_occurence(mes))
+
+# liste = [(ord(char) + 2) for char in mes ]
+# listes = ''.join([(chr(char)) for char in liste ])
+# print(listes)
